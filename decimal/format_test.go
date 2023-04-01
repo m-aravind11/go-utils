deaec086e22e67a2282ff0d06ca5969ec800b6c6
Aravind <maravind2000@gmail.com>
2023-04-01 23:34:49 +0530

    Verify errors in testcases

--- decimal/format_test.go
+++ decimal/format_test.go
@@ -1,6 +1,7 @@
 package decimal
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 )
@@ -10,60 +11,61 @@ func TestRoundAndFormat(t *testing.T) {
 		val     interface{}
 		places  int32
 		want    string
-		wantErr bool
+		wantErr error
 	}{
 		// Basic test cases
-		{1.234, 2, "1.23", false},
-		{1.235, 2, "1.24", false},
-		{1.236, 2, "1.24", false},
-		{1.225, 2, "1.23", false},
-		{1.215, 2, "1.22", false},
-		{1.225, 1, "1.2", false},
-		{1.234, 0, "1", false},
-		{1234, 2, "1234.00", false},
-		{float32(1.234), 2, "1.23", false},
-		{"1.234", 2, "1.23", false},
-		{"1", 2, "1.00", false},
-		{"1.2345", 2, "1.23", false},
-		{"-1.234", 2, "-1.23", false},
-		{"1.239", 0, "1", false},
-		{"-1.239", 0, "-1", false},
-		{0, 2, "0.00", false},
+		{1.234, 2, "1.23", nil},
+		{1.235, 2, "1.24", nil},
+		{1.236, 2, "1.24", nil},
+		{1.225, 2, "1.23", nil},
+		{1.215, 2, "1.22", nil},
+		{1.225, 1, "1.2", nil},
+		{1.234, 0, "1", nil},
+		{1234, 2, "1234.00", nil},
+		{float32(1.234), 2, "1.23", nil},
+		{"1.234", 2, "1.23", nil},
+		{"1", 2, "1.00", nil},
+		{"1.2345", 2, "1.23", nil},
+		{"-1.234", 2, "-1.23", nil},
+		{"1.239", 0, "1", nil},
+		{"-1.239", 0, "-1", nil},
+		{0, 2, "0.00", nil},
 
 		// Test cases with padding/trimming zeros
-		{1.2, 4, "1.2000", false},
-		{1.234, 4, "1.2340", false},
-		{1.200, 4, "1.2000", false},
-		{1.000, 4, "1.0000", false},
-		{1, 4, "1.0000", false},
-		{"1.2", 4, "1.2000", false},
-		{"1.234", 4, "1.2340", false},
-		{"1.200", 4, "1.2000", false},
-		{"1.000", 4, "1.0000", false},
-		{"1", 4, "1.0000", false},
-		{"-1.2", 4, "-1.2000", false},
-		{"-1.234", 4, "-1.2340", false},
-		{"-1.200", 4, "-1.2000", false},
-		{"-1.000", 4, "-1.0000", false},
-		{"-1", 4, "-1.0000", false},
-		{"0", 4, "0.0000", false},
+		{1.2, 4, "1.2000", nil},
+		{1.234, 4, "1.2340", nil},
+		{1.200, 4, "1.2000", nil},
+		{1.000, 4, "1.0000", nil},
+		{1, 4, "1.0000", nil},
+		{"1.2", 4, "1.2000", nil},
+		{"1.234", 4, "1.2340", nil},
+		{"1.200", 4, "1.2000", nil},
+		{"1.000", 4, "1.0000", nil},
+		{"1", 4, "1.0000", nil},
+		{"-1.2", 4, "-1.2000", nil},
+		{"-1.234", 4, "-1.2340", nil},
+		{"-1.200", 4, "-1.2000", nil},
+		{"-1.000", 4, "-1.0000", nil},
+		{"-1", 4, "-1.0000", nil},
+		{"0", 4, "0.0000", nil},
 
 		// Error test cases
-		{"invalid", 2, "", true},
-		{complex(1, 2), 2, "", true},
+		{"invalid", 2, "", errors.New("strconv.ParseFloat: parsing \"invalid\": invalid syntax")},
+		{complex(1, 2), 2, "", errors.New("value is not a float or string")},
 	}
 
-	for _, tt := range tests {
-		t.Run(fmt.Sprintf("TestRoundAndFormat(%v, %d)", tt.val, tt.places), func(t *testing.T) {
-			got, err := RoundAndFormat(tt.val, tt.places)
+	for _, input := range tests {
+		t.Run(fmt.Sprintf("TestRoundAndFormat(%v, %d)", input.val, input.places), func(t *testing.T) {
+			got, err := RoundAndFormat(input.val, input.places)
 
-			if err != nil && !tt.wantErr {
-				t.Errorf("TestRoundAndFormat(%v, %d) got error = %v, wantErr %v", tt.val, tt.places, err, tt.wantErr)
-				return
+			if err != nil {
+				if input.wantErr == nil || input.wantErr.Error() != err.Error() {
+					t.Errorf("TestRoundAndFormat(%v, %d) got error = %v, wantErr %v", input.val, input.places, err, input.wantErr)
+				}
 			}
 
-			if tt.want != got {
-				t.Errorf("TestRoundAndFormat(%v, %d) = %v, want %v", tt.val, tt.places, got, tt.want)
+			if input.want != got {
+				t.Errorf("TestRoundAndFormat(%v, %d) = %v, want %v", input.val, input.places, got, input.want)
 			}
 		})
 	}
@@ -74,60 +76,61 @@ func TestCeilAndFormat(t *testing.T) {
 		val     interface{}
 		places  int32
 		want    string
-		wantErr bool
+		wantErr error
 	}{
 		// Basic test cases
-		{1.234, 2, "1.24", false},
-		{1.235, 2, "1.24", false},
-		{1.236, 2, "1.24", false},
-		{1.225, 2, "1.23", false},
-		{1.215, 2, "1.22", false},
-		{1.225, 1, "1.3", false},
-		{1.234, 0, "2", false},
-		{1234, 2, "1234.00", false},
-		{float32(1.234), 2, "1.24", false},
-		{"1.234", 2, "1.24", false},
-		{"1", 2, "1.00", false},
-		{"1.2345", 2, "1.24", false},
-		{"-1.234", 2, "-1.23", false},
-		{"1.239", 0, "2", false},
-		{"-1.239", 0, "-1", false},
-		{0, 2, "0.00", false},
+		{1.234, 2, "1.24", nil},
+		{1.235, 2, "1.24", nil},
+		{1.236, 2, "1.24", nil},
+		{1.225, 2, "1.23", nil},
+		{1.215, 2, "1.22", nil},
+		{1.225, 1, "1.3", nil},
+		{1.234, 0, "2", nil},
+		{1234, 2, "1234.00", nil},
+		{float32(1.234), 2, "1.24", nil},
+		{"1.234", 2, "1.24", nil},
+		{"1", 2, "1.00", nil},
+		{"1.2345", 2, "1.24", nil},
+		{"-1.234", 2, "-1.23", nil},
+		{"1.239", 0, "2", nil},
+		{"-1.239", 0, "-1", nil},
+		{0, 2, "0.00", nil},
 
 		// Test cases with padding/trimming zeros
-		{1.2, 4, "1.2000", false},
-		{1.234, 4, "1.2340", false},
-		{1.200, 4, "1.2000", false},
-		{1.000, 4, "1.0000", false},
-		{1, 4, "1.0000", false},
-		{"1.2", 4, "1.2000", false},
-		{"1.234", 4, "1.2340", false},
-		{"1.200", 4, "1.2000", false},
-		{"1.000", 4, "1.0000", false},
-		{"1", 4, "1.0000", false},
-		{"-1.2", 4, "-1.2000", false},
-		{"-1.234", 4, "-1.2340", false},
-		{"-1.200", 4, "-1.2000", false},
-		{"-1.000", 4, "-1.0000", false},
-		{"-1", 4, "-1.0000", false},
-		{"0", 4, "0.0000", false},
+		{1.2, 4, "1.2000", nil},
+		{1.234, 4, "1.2340", nil},
+		{1.200, 4, "1.2000", nil},
+		{1.000, 4, "1.0000", nil},
+		{1, 4, "1.0000", nil},
+		{"1.2", 4, "1.2000", nil},
+		{"1.234", 4, "1.2340", nil},
+		{"1.200", 4, "1.2000", nil},
+		{"1.000", 4, "1.0000", nil},
+		{"1", 4, "1.0000", nil},
+		{"-1.2", 4, "-1.2000", nil},
+		{"-1.234", 4, "-1.2340", nil},
+		{"-1.200", 4, "-1.2000", nil},
+		{"-1.000", 4, "-1.0000", nil},
+		{"-1", 4, "-1.0000", nil},
+		{"0", 4, "0.0000", nil},
 
 		// Error test cases
-		{"invalid", 2, "", true},
-		{complex(1, 2), 2, "", true},
+		{"invalid", 2, "", errors.New("strconv.ParseFloat: parsing \"invalid\": invalid syntax")},
+		{complex(1, 2), 2, "", errors.New("value is not a float or string")},
 	}
 
-	for _, tt := range tests {
-		t.Run(fmt.Sprintf("TestCeilAndFormat(%v, %d)", tt.val, tt.places), func(t *testing.T) {
-			got, err := CeilAndFormat(tt.val, tt.places)
+	for _, input := range tests {
+		t.Run(fmt.Sprintf("TestCeilAndFormat(%v, %d)", input.val, input.places), func(t *testing.T) {
+			got, err := CeilAndFormat(input.val, input.places)
 
-			if err != nil && !tt.wantErr {
-				t.Errorf("TestCeilAndFormat(%v, %d) got error = %v, wantErr %v", tt.val, tt.places, err, tt.wantErr)
-				return
+			if err != nil {
+				if input.wantErr == nil || input.wantErr.Error() != err.Error() {
+					t.Errorf("TestCeilAndFormat(%v, %d) got error = %v, wantErr %v", input.val, input.places, err, input.wantErr)
+				}
 			}
 
-			if tt.want != got {
-				t.Errorf("TestCeilAndFormat(%v, %d) = %v, want %v", tt.val, tt.places, got, tt.want)
+			if input.want != got {
+				t.Errorf("TestCeilAndFormat(%v, %d) = %v, want %v", input.val, input.places, got, input.want)
 			}
 		})
 	}
@@ -138,60 +141,61 @@ func TestFloorAndFormat(t *testing.T) {
 		val     interface{}
 		places  int32
 		want    string
-		wantErr bool
+		wantErr error
 	}{
 		// Basic test cases
-		{1.234, 2, "1.23", false},
-		{1.235, 2, "1.23", false},
-		{1.236, 2, "1.23", false},
-		{1.225, 2, "1.22", false},
-		{1.215, 2, "1.21", false},
-		{1.225, 1, "1.2", false},
-		{1.234, 0, "1", false},
-		{1234, 2, "1234.00", false},
-		{float32(1.234), 2, "1.23", false},
-		{"1.234", 2, "1.23", false},
-		{"1", 2, "1.00", false},
-		{"1.2345", 2, "1.23", false},
-		{"-1.234", 2, "-1.24", false},
-		{"1.239", 0, "1", false},
-		{"-1.239", 0, "-2", false},
-		{0, 2, "0.00", false},
+		{1.234, 2, "1.23", nil},
+		{1.235, 2, "1.23", nil},
+		{1.236, 2, "1.23", nil},
+		{1.225, 2, "1.22", nil},
+		{1.215, 2, "1.21", nil},
+		{1.225, 1, "1.2", nil},
+		{1.234, 0, "1", nil},
+		{1234, 2, "1234.00", nil},
+		{float32(1.234), 2, "1.23", nil},
+		{"1.234", 2, "1.23", nil},
+		{"1", 2, "1.00", nil},
+		{"1.2345", 2, "1.23", nil},
+		{"-1.234", 2, "-1.24", nil},
+		{"1.239", 0, "1", nil},
+		{"-1.239", 0, "-2", nil},
+		{0, 2, "0.00", nil},
 
 		// Test cases with padding/trimming zeros
-		{1.2, 4, "1.2000", false},
-		{1.234, 4, "1.2340", false},
-		{1.200, 4, "1.2000", false},
-		{1.000, 4, "1.0000", false},
-		{1, 4, "1.0000", false},
-		{"1.2", 4, "1.2000", false},
-		{"1.234", 4, "1.2340", false},
-		{"1.200", 4, "1.2000", false},
-		{"1.000", 4, "1.0000", false},
-		{"1", 4, "1.0000", false},
-		{"-1.2", 4, "-1.2000", false},
-		{"-1.234", 4, "-1.2340", false},
-		{"-1.200", 4, "-1.2000", false},
-		{"-1.000", 4, "-1.0000", false},
-		{"-1", 4, "-1.0000", false},
-		{"0", 4, "0.0000", false},
+		{1.2, 4, "1.2000", nil},
+		{1.234, 4, "1.2340", nil},
+		{1.200, 4, "1.2000", nil},
+		{1.000, 4, "1.0000", nil},
+		{1, 4, "1.0000", nil},
+		{"1.2", 4, "1.2000", nil},
+		{"1.234", 4, "1.2340", nil},
+		{"1.200", 4, "1.2000", nil},
+		{"1.000", 4, "1.0000", nil},
+		{"1", 4, "1.0000", nil},
+		{"-1.2", 4, "-1.2000", nil},
+		{"-1.234", 4, "-1.2340", nil},
+		{"-1.200", 4, "-1.2000", nil},
+		{"-1.000", 4, "-1.0000", nil},
+		{"-1", 4, "-1.0000", nil},
+		{"0", 4, "0.0000", nil},
 
 		// Error test cases
-		{"invalid", 2, "", true},
-		{complex(1, 2), 2, "", true},
+		{"invalid", 2, "", errors.New("strconv.ParseFloat: parsing \"invalid\": invalid syntax")},
+		{complex(1, 2), 2, "", errors.New("value is not a float or string")},
 	}
 
-	for _, tt := range tests {
-		t.Run(fmt.Sprintf("TestFloorAndFormat(%v, %d)", tt.val, tt.places), func(t *testing.T) {
-			got, err := FloorAndFormat(tt.val, tt.places)
+	for _, input := range tests {
+		t.Run(fmt.Sprintf("TestFloorAndFormat(%v, %d)", input.val, input.places), func(t *testing.T) {
+			got, err := FloorAndFormat(input.val, input.places)
 
-			if err != nil && !tt.wantErr {
-				t.Errorf("TestFloorAndFormat(%v, %d) got error = %v, wantErr %v", tt.val, tt.places, err, tt.wantErr)
-				return
+			if err != nil {
+				if input.wantErr == nil || input.wantErr.Error() != err.Error() {
+					t.Errorf("TestCeilAndFormat(%v, %d) got error = %v, wantErr %v", input.val, input.places, err, input.wantErr)
+				}
 			}
 
-			if tt.want != got {
-				t.Errorf("TestFloorAndFormat(%v, %d) = %v, want %v", tt.val, tt.places, got, tt.want)
+			if input.want != got {
+				t.Errorf("TestFloorAndFormat(%v, %d) = %v, want %v", input.val, input.places, got, input.want)
 			}
 		})
 	}
--- decimal/round_test.go
+++ decimal/round_test.go
@@ -1,6 +1,7 @@
 package decimal
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 )
@@ -10,47 +11,48 @@ func TestRound(t *testing.T) {
 		val     interface{}
 		places  int32
 		want    float64
-		wantErr bool
+		wantErr error
 	}{
 		// Test cases for float32 input
-		{val: float32(1.234), places: 2, want: 1.23, wantErr: false},
-		{val: float32(3.14159), places: 2, want: 3.14, wantErr: false},
-		{val: float32(3.1459), places: 1, want: 3.1, wantErr: false},
-		{val: float32(-3.1459), places: 2, want: -3.15, wantErr: false},
-		{val: float32(3.1459), places: -1, want: 3, wantErr: false},
+		{val: float32(1.234), places: 2, want: 1.23, wantErr: nil},
+		{val: float32(3.14159), places: 2, want: 3.14, wantErr: nil},
+		{val: float32(3.1459), places: 1, want: 3.1, wantErr: nil},
+		{val: float32(-3.1459), places: 2, want: -3.15, wantErr: nil},
+		{val: float32(3.1459), places: -1, want: 3, wantErr: nil},
 
 		// Test cases for float64 input
-		{val: float64(1.234), places: 2, want: 1.23, wantErr: false},
-		{val: 3.14159, places: 2, want: 3.14, wantErr: false},
-		{val: 3.1459, places: 1, want: 3.1, wantErr: false},
-		{val: -3.1459, places: 2, want: -3.15, wantErr: false},
-		{val: 3.1459, places: -1, want: 3, wantErr: false},
+		{val: float64(1.234), places: 2, want: 1.23, wantErr: nil},
+		{val: 3.14159, places: 2, want: 3.14, wantErr: nil},
+		{val: 3.1459, places: 1, want: 3.1, wantErr: nil},
+		{val: -3.1459, places: 2, want: -3.15, wantErr: nil},
+		{val: 3.1459, places: -1, want: 3, wantErr: nil},
 
 		// Test cases for string input
-		{val: "3.14159", places: 2, want: 3.14, wantErr: false},
-		{val: "3.1459", places: 1, want: 3.1, wantErr: false},
-		{val: "-3.1459", places: 2, want: -3.15, wantErr: false},
-		{val: "3.1459", places: -1, want: 3, wantErr: false},
-		{val: "1.234", places: 2, want: 1.23, wantErr: false},
-		{val: "1.234", places: -2, want: 1, wantErr: false},
-		{val: "invalid", places: 2, want: 0, wantErr: true},
-		{val: "not a float", places: 2, wantErr: true},
+		{val: "3.14159", places: 2, want: 3.14, wantErr: nil},
+		{val: "3.1459", places: 1, want: 3.1, wantErr: nil},
+		{val: "-3.1459", places: 2, want: -3.15, wantErr: nil},
+		{val: "3.1459", places: -1, want: 3, wantErr: nil},
+		{val: "1.234", places: 2, want: 1.23, wantErr: nil},
+		{val: "1.234", places: -2, want: 1, wantErr: nil},
+		{val: "invalid", places: 2, want: 0, wantErr: errors.New("strconv.ParseFloat: parsing \"invalid\": invalid syntax")},
+		{val: "not a float", places: 2, wantErr: errors.New("strconv.ParseFloat: parsing \"not a float\": invalid syntax")},
 
 		// Nil input
-		{val: nil, places: 2, want: 0, wantErr: true},
+		{val: nil, places: 2, want: 0, wantErr: errors.New("value is not a float or string")},
 	}
 
-	for _, tt := range tests {
-		t.Run(fmt.Sprintf("Round(%v, %d)", tt.val, tt.places), func(t *testing.T) {
-			got, err := Round(tt.val, tt.places)
+	for _, input := range tests {
+		t.Run(fmt.Sprintf("Round(%v, %d)", input.val, input.places), func(t *testing.T) {
+			got, err := Round(input.val, input.places)
 
-			if err != nil && !tt.wantErr {
-				t.Errorf("Round(%v, %d) got error = %v, wantErr %v", tt.val, tt.places, err, tt.wantErr)
-				return
+			if err != nil {
+				if input.wantErr == nil || input.wantErr.Error() != err.Error() {
+					t.Errorf("Round(%v, %d) got error = %v, wantErr %v", input.val, input.places, err, input.wantErr)
+				}
 			}
 
-			if tt.want != got {
-				t.Errorf("Round(%v, %d) = %v, want %v", tt.val, tt.places, got, tt.want)
+			if input.want != got {
+				t.Errorf("Round(%v, %d) = %v, want %v", input.val, input.places, got, input.want)
 			}
 		})
 	}
@@ -61,47 +63,48 @@ func TestCeil(t *testing.T) {
 		val     interface{}
 		places  int32
 		want    float64
-		wantErr bool
+		wantErr error
 	}{
 		// Test cases for float32 input
-		{val: float32(1.234), places: 2, want: 1.24, wantErr: false},
-		{val: float32(3.14159), places: 2, want: 3.15, wantErr: false},
-		{val: float32(3.1459), places: 1, want: 3.2, wantErr: false},
-		{val: float32(-3.1459), places: 2, want: -3.14, wantErr: false},
-		{val: float32(3.1459), places: -1, want: 4, wantErr: false},
+		{val: float32(1.234), places: 2, want: 1.24, wantErr: nil},
+		{val: float32(3.14159), places: 2, want: 3.15, wantErr: nil},
+		{val: float32(3.1459), places: 1, want: 3.2, wantErr: nil},
+		{val: float32(-3.1459), places: 2, want: -3.14, wantErr: nil},
+		{val: float32(3.1459), places: -1, want: 4, wantErr: nil},
 
 		// Test cases for float64 input
-		{val: float64(1.234), places: 2, want: 1.24, wantErr: false},
-		{val: 3.14159, places: 2, want: 3.15, wantErr: false},
-		{val: 3.1459, places: 1, want: 3.2, wantErr: false},
-		{val: -3.1459, places: 2, want: -3.14, wantErr: false},
-		{val: 3.1459, places: -1, want: 4, wantErr: false},
+		{val: float64(1.234), places: 2, want: 1.24, wantErr: nil},
+		{val: 3.14159, places: 2, want: 3.15, wantErr: nil},
+		{val: 3.1459, places: 1, want: 3.2, wantErr: nil},
+		{val: -3.1459, places: 2, want: -3.14, wantErr: nil},
+		{val: 3.1459, places: -1, want: 4, wantErr: nil},
 
 		// Test cases for string input
-		{val: "3.14159", places: 2, want: 3.15, wantErr: false},
-		{val: "3.1459", places: 1, want: 3.2, wantErr: false},
-		{val: "-3.1459", places: 2, want: -3.14, wantErr: false},
-		{val: "3.1459", places: -1, want: 4, wantErr: false},
-		{val: "1.234", places: 2, want: 1.24, wantErr: false},
-		{val: "1.234", places: -2, want: 2, wantErr: false},
-		{val: "invalid", places: 2, want: 0, wantErr: true},
-		{val: "not a float", places: 2, wantErr: true},
+		{val: "3.14159", places: 2, want: 3.15, wantErr: nil},
+		{val: "3.1459", places: 1, want: 3.2, wantErr: nil},
+		{val: "-3.1459", places: 2, want: -3.14, wantErr: nil},
+		{val: "3.1459", places: -1, want: 4, wantErr: nil},
+		{val: "1.234", places: 2, want: 1.24, wantErr: nil},
+		{val: "1.234", places: -2, want: 2, wantErr: nil},
+		{val: "invalid", places: 2, want: 0, wantErr: errors.New("strconv.ParseFloat: parsing \"invalid\": invalid syntax")},
+		{val: "not a float", places: 2, wantErr: errors.New("strconv.ParseFloat: parsing \"not a float\": invalid syntax")},
 
 		// Nil input
-		{val: nil, places: 2, want: 0, wantErr: true},
+		{val: nil, places: 2, want: 0, wantErr: errors.New("value is not a float or string")},
 	}
 
-	for _, tt := range tests {
-		t.Run(fmt.Sprintf("TestCeil(%v, %d)", tt.val, tt.places), func(t *testing.T) {
-			got, err := Ceil(tt.val, tt.places)
+	for _, input := range tests {
+		t.Run(fmt.Sprintf("TestCeil(%v, %d)", input.val, input.places), func(t *testing.T) {
+			got, err := Ceil(input.val, input.places)
 
-			if err != nil && !tt.wantErr {
-				t.Errorf("TestCeil(%v, %d) got error = %v, wantErr %v", tt.val, tt.places, err, tt.wantErr)
-				return
+			if err != nil {
+				if input.wantErr == nil || input.wantErr.Error() != err.Error() {
+					t.Errorf("TestCeil(%v, %d) got error = %v, wantErr %v", input.val, input.places, err, input.wantErr)
+				}
 			}
 
-			if tt.want != got {
-				t.Errorf("TestCeil(%v, %d) = %v, want %v", tt.val, tt.places, got, tt.want)
+			if input.want != got {
+				t.Errorf("TestCeil(%v, %d) = %v, want %v", input.val, input.places, got, input.want)
 			}
 		})
 	}
@@ -112,47 +115,48 @@ func TestFloor(t *testing.T) {
 		val     interface{}
 		places  int32
 		want    float64
-		wantErr bool
+		wantErr error
 	}{
 		// Test cases for float32 input
-		{val: float32(1.234), places: 2, want: 1.23, wantErr: false},
-		{val: float32(3.14159), places: 2, want: 3.14, wantErr: false},
-		{val: float32(3.1459), places: 1, want: 3.1, wantErr: false},
-		{val: float32(-3.1459), places: 2, want: -3.15, wantErr: false},
-		{val: float32(3.1459), places: -1, want: 3, wantErr: false},
+		{val: float32(1.234), places: 2, want: 1.23, wantErr: nil},
+		{val: float32(3.14159), places: 2, want: 3.14, wantErr: nil},
+		{val: float32(3.1459), places: 1, want: 3.1, wantErr: nil},
+		{val: float32(-3.1459), places: 2, want: -3.15, wantErr: nil},
+		{val: float32(3.1459), places: -1, want: 3, wantErr: nil},
 
 		// Test cases for float64 input
-		{val: float64(1.234), places: 2, want: 1.23, wantErr: false},
-		{val: 3.14159, places: 2, want: 3.14, wantErr: false},
-		{val: 3.1459, places: 1, want: 3.1, wantErr: false},
-		{val: -3.1459, places: 2, want: -3.15, wantErr: false},
-		{val: 3.1459, places: -1, want: 3, wantErr: false},
+		{val: float64(1.234), places: 2, want: 1.23, wantErr: nil},
+		{val: 3.14159, places: 2, want: 3.14, wantErr: nil},
+		{val: 3.1459, places: 1, want: 3.1, wantErr: nil},
+		{val: -3.1459, places: 2, want: -3.15, wantErr: nil},
+		{val: 3.1459, places: -1, want: 3, wantErr: nil},
 
 		// Test cases for string input
-		{val: "3.14159", places: 2, want: 3.14, wantErr: false},
-		{val: "3.1459", places: 1, want: 3.1, wantErr: false},
-		{val: "-3.1459", places: 2, want: -3.15, wantErr: false},
-		{val: "3.1459", places: -1, want: 3, wantErr: false},
-		{val: "1.234", places: 2, want: 1.23, wantErr: false},
-		{val: "1.234", places: -2, want: 1, wantErr: false},
-		{val: "invalid", places: 2, want: 0, wantErr: true},
-		{val: "not a float", places: 2, wantErr: true},
+		{val: "3.14159", places: 2, want: 3.14, wantErr: nil},
+		{val: "3.1459", places: 1, want: 3.1, wantErr: nil},
+		{val: "-3.1459", places: 2, want: -3.15, wantErr: nil},
+		{val: "3.1459", places: -1, want: 3, wantErr: nil},
+		{val: "1.234", places: 2, want: 1.23, wantErr: nil},
+		{val: "1.234", places: -2, want: 1, wantErr: nil},
+		{val: "invalid", places: 2, want: 0, wantErr: errors.New("strconv.ParseFloat: parsing \"invalid\": invalid syntax")},
+		{val: "not a float", places: 2, wantErr: errors.New("strconv.ParseFloat: parsing \"not a float\": invalid syntax")},
 
 		// Nil input
-		{val: nil, places: 2, want: 0, wantErr: true},
+		{val: nil, places: 2, want: 0, wantErr: errors.New("value is not a float or string")},
 	}
 
-	for _, tt := range tests {
-		t.Run(fmt.Sprintf("TestFloor(%v, %d)", tt.val, tt.places), func(t *testing.T) {
-			got, err := Floor(tt.val, tt.places)
+	for _, input := range tests {
+		t.Run(fmt.Sprintf("TestFloor(%v, %d)", input.val, input.places), func(t *testing.T) {
+			got, err := Floor(input.val, input.places)
 
-			if err != nil && !tt.wantErr {
-				t.Errorf("TestFloor(%v, %d) got error = %v, wantErr %v", tt.val, tt.places, err, tt.wantErr)
-				return
+			if err != nil {
+				if input.wantErr == nil || input.wantErr.Error() != err.Error() {
+					t.Errorf("TestCeil(%v, %d) got error = %v, wantErr %v", input.val, input.places, err, input.wantErr)
+				}
 			}
 
-			if tt.want != got {
-				t.Errorf("TestFloor(%v, %d) = %v, want %v", tt.val, tt.places, got, tt.want)
+			if input.want != got {
+				t.Errorf("TestFloor(%v, %d) = %v, want %v", input.val, input.places, got, input.want)
 			}
 		})
 	}
